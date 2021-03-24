0d0ce4de0a82a9982ede25f9a9f6ca7832d18700
BastLast <bastien.last87@gmail.com>
2021-03-24 14:56:46 +0100

    connexion à la bdd

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const express = require('express');
-
+const {Sequelize} = require("sequelize");
 const app = express();
+
+(async () => {
+
+    const sequelize = new Sequelize({
+        dialect: "sqlite",
+        storage: "database.sqlite"
+    });
+
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+})();
+
 
 app.get('/', (req, res) => {
     res.json({
